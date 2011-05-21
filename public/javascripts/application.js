f62ecdecc4c8e61063443f424ace63c929dbc006
Daniel Persson <gudski@gmail.com>
2011-05-21 14:32:30 +0200

    add button style with toogle ability

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,31 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var AGILE = (function(){
+	
+	var log = function(item){
+		if(window.console && window.console.log){
+			window.console.log(item);
+		}
+	}
+	
+	var setupToggle = function()	{
+		$(".toggle-block").click(function(){		
+				var selectorToToggle = $(this).attr("data-toogle");		
+				$(selectorToToggle).toggle();					
+				return false;
+		});
+	}
+	
+	init = function()	{
+		//init in jquery context
+		$(function(){
+		
+			setupToggle();			
+			
+		});
+	}
+	
+	
+	return this.init();	
+	
+})();
