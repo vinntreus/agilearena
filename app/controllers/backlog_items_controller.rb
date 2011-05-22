5fabe65d5f20abb0bac615057cf4cc184e036d7f
Daniel Persson <gudski@gmail.com>
2011-05-22 23:39:58 +0200

    init ajaxify add and delete backlogitems

--- app/controllers/backlog_items_controller.rb
+++ app/controllers/backlog_items_controller.rb
@@ -1,4 +1,6 @@
 class BacklogItemsController < ApplicationController
+	include ActionView::Helpers::DateHelper
+
 	before_filter :authenticate, :only => [:create, :destroy]
 	
 	def create
@@ -12,14 +14,26 @@ class BacklogItemsController < ApplicationController
 		@backlog_item = @backlog.backlog_items.build(params[:backlog_item])
 		
 		if @backlog_item.save
-			flash[:success] = "Created backlogitem"
-			redirect_to @backlog
+			render :json => { :id => @backlog_item.id, :created => time_ago_in_words(@backlog_item.created_at) }
+			#flash[:success] = "Created backlogitem"
+			#respond_to do |format|
+			#	format.html redirect_to @backlog
+			#	format.json { render :json => @backlog_item }
+			#end
+
 		else
-      flash[:error] = "Could not create backlogitem!"
-      redirect_to root_path
+			render :json => { :error => "Could not create backlogitem" }
+      #flash[:error] = "Could not create backlogitem!"
+      #redirect_to root_path
     end
+    
+
+
 	end
 	
 	def destroy
+		BacklogItem.delete(params[:id])
+
+		render :json => {}
 	end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,6 +8,13 @@ var AGILE = (function(){
 		}
 	}
 	
+		
+	var dir = function(item){
+		if(window.console && window.console.dir){
+			window.console.dir(item);
+		}
+	}
+	
 	var setupToggle = function()	{
 		$(".toggle-block").click(function(){		
 				var selectorToToggle = $(this).attr("data-toogle");		
@@ -16,12 +23,86 @@ var AGILE = (function(){
 		});
 	}
 	
+	var backlogItem = {
+		form : null,
+		list : null,
+		item : "<li class='backlog-item' data-id='#i#'><a href='#' class='button right delete'>Delete</a><p class='title'>#t#</p><p class='timestamp'>Created #c# ago.</p></li>",
+		init : function()	{
+			this.form = this.form || $("#new_backlog_item");
+			this.list = this.list || $("#backlog-items-list");
+			if(this.form.length > 0){
+				this.setupForm();
+			}
+			if(this.list.length > 0){
+				this.setupList();
+			}
+		},
+		setupList: function(){
+			this.list.click(function(e){
+				var clickedOn = $(e.target);
+				if(clickedOn.is("a") && clickedOn.hasClass("delete"))
+				{
+					var currentItem =	clickedOn.parent();
+					$.ajax({
+						type: "DELETE",
+						url : "/backlog_items/" + currentItem.data("id"),
+						success : function(data, textStatus, jqXHR){
+							currentItem.remove();
+						},
+						error : function(jqXHR, textStatus, errorThrown){
+							log("error");
+						}
+					});
+					return false;
+				}
+			});
+		},
+		getFormData : function(){
+			var data = {};
+			$("input", this.form).each(function(){
+				var field = $(this);
+				data[field.attr("name")] = field.val();
+			});
+			return data;
+		},
+		setupForm : function(){
+			var that = this;
+			this.form.submit(function(e)	{
+				var d = that.getFormData();
+				$.ajax({
+					type: "POST",
+					data : d,
+					url : that.form.attr("action"),
+					success : function(data, textStatus, jqXHR){
+						var item = that.item.replace(/#t#/, d["backlog_item[title]"]);
+						item = item.replace(/#i#/, data.id);
+						item = item.replace(/#c#/, data.created);
+						$("#backlog-items-list").append(item);
+						that.clearForm();
+					},
+					error : function(jqXHR, textStatus, errorThrown){
+						log("error");
+					}
+				});
+				return false;
+			});
+		},
+		clearForm : function(){
+			var inputs = $("input[type='text']", this.form);
+			inputs.each(function(){
+				var field = $(this);
+				field.val("");
+			});
+			inputs[0].focus();
+		}		
+	}
+	
 	init = function()	{
 		//init in jquery context
 		$(function(){
 		
 			setupToggle();			
-			
+			backlogItem.init();
 		});
 	}
 	
